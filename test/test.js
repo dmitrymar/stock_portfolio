878c035adee911499fcfc552453490d94bd1e26c
Dmitry Mardukhayev <dmitrymar@yahoo.com>
2016-12-03 15:42:20 -0500

    added test cases

--- test/test.js
+++ test/test.js
@@ -1,8 +1,42 @@
+/*
+ * describe: either portfolio is empty or has stocks
+	* Verify symbol data is available after form submission
+ 	* should not add empty stock
+	* shouldn't make ajax request if symbol is found
+ * describe: 'having no stocks in Portfolio'
+	* should not have any stock in portfolio on start
+ * describe: having some saved Stocks
+	* should save Portfolios to db
+ 	* should save a maximum of 5 stocks
+ 	* should remove a stock from portfolio by clicking remove
+	* share value should update
+	* clicking 'add to portfolio' button add symbol to portfolio
+	* clicking 'add to portfolio' button adds shares to portfolio
+	* a maximum of 5 symbols can be added to portfolio
+	* submitting empty shares sets share to 0
+	* Save symbol and share value to db
+	* last trade price for each stock in portfio updates every 5 secons
+	* indicate if price went up or down for every stock
+*/
+
+
+describe("either portfolio is empty or has stocks", function() {
+  beforeEach(module('portfolio'));
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+	expect(scope.todos.length).toBe(0);
+    expect(a).toBe(true);
+  });
+});
+
+
 describe("Hello World example", function() {
 
 	  beforeEach(module('portfolio'));
 
-	  var HelloWorldController, 
+	  var HelloWorldController,
 		  scope;
 
 	  beforeEach(inject(function ($rootScope, $controller) {
@@ -16,4 +50,4 @@ describe("Hello World example", function() {
 		      expect(scope.greeting).toEqual("Hello World!");
 		    });
 });
- 
+
